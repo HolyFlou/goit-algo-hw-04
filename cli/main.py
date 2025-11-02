c3df4839a621e6f5774d0f7531fd10cdaa08cee5
HolyFlou <stepanenko.nekita2020@gmail.com>
2025-11-02 21:24:31 +0200

    dz 4

---
+++ cli/main.py
@@ -0,0 +1,53 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    except ValueError:
+        return "You need to write more arguments."
+
+def change_contact(args, contacts):
+    try:
+        name, phone = args
+        if name in contacts:
+            contacts[name] = phone
+            return "Contact changed."
+        else:
+            return "Contact is not in array."
+    except ValueError:
+        return "You need to write more arguments."
+def all_contact(name, phone):
+    return f"Name: {name}, Phone: {phone}"
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "all":
+            if not len(contacts):
+                print("Contacts are empty")
+            else:
+                for name, phone in contacts.items():
+                    print(all_contact(name, phone))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
